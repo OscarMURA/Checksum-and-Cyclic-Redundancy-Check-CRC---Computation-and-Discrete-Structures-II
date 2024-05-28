7e325ecdbcd50778a44d620788ca425eb3f4da00
Lrojas898 <luis.manuel.rojas71@gmail.com>
2024-05-27 20:41:59 -0500

    Data generated for chekcusm and crc

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -11,23 +11,25 @@ def main(): Unit = {
   //ReportTI.createUnitData(DataType.MEDIUM, 20)
   //ReportTI.createUnitData(DataType.BIG, 20)
 
-  ReportTI.testReportGeneric(DataType.TOY, 25, TypeEDC.DUAL_SUM)
-  ReportTI.testReportGeneric(DataType.SMALL, 25, TypeEDC.DUAL_SUM)
-  ReportTI.testReportGeneric(DataType.TOY, 25, TypeEDC.CRC)
-  ReportTI.testReportGeneric(DataType.SMALL, 25, TypeEDC.CRC)
+  //ReportTI.testReportGeneric(DataType.TOY, 25, TypeEDC.DUAL_SUM)
+  //ReportTI.testReportGeneric(DataType.SMALL, 25, TypeEDC.DUAL_SUM)
+  //ReportTI.testReportGeneric(DataType.TOY, 25, TypeEDC.CRC)
+  //ReportTI.testReportGeneric(DataType.SMALL, 25, TypeEDC.CRC)
 
 
 
-  ReportTI.testReportGeneric(DataType.MEDIUM, 20, TypeEDC.DUAL_SUM)
-  ReportTI.testReportGeneric(DataType.MEDIUM, 20, TypeEDC.CRC)
+  //ReportTI.testReportGeneric(DataType.MEDIUM, 20, TypeEDC.DUAL_SUM)
+ // ReportTI.testReportGeneric(DataType.MEDIUM, 20, TypeEDC.CRC)
 
-  ReportTI.testByFileCRC("1")
-  ReportTI.testByFileCRC("2")
-  ReportTI.testByFileCRC("3")
-  ReportTI.testByFileCRC("4")
-  ReportTI.testByFileCRC("5")
+  //ReportTI.testByFileCRC("1")
+  //ReportTI.testByFileCRC("2")
+  //ReportTI.testByFileCRC("3")
+  //ReportTI.testByFileCRC("4")
+  //ReportTI.testByFileCRC("5")
 
-  ReportTI.testReportGeneric(DataType.BIG, 10, TypeEDC.DUAL_SUM)
+  //ReportTI.testReportGeneric(DataType.BIG, 10, TypeEDC.DUAL_SUM)
+  ReportTI.testReportGeneric(DataType.BIG, 10, TypeEDC.CRC)
+
   print("Archivo de datos  generado.  ")
   println("Proceso completado.")
 }
